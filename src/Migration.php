0579cdd7358b46a5e3371473c28c5fdc7fb3a003
pokoot <pokoot@gmail.com>
2014-11-15 21:35:15 +0800

    migration getCommands function added

---
+++ src/Migration.php
@@ -0,0 +1,33 @@
+<?php namespace Groovey\Migration;
+
+use Groovey\Migration\Adapters\Adapter;
+
+class Migration
+{
+    public $adapter;
+
+    public function __construct(Adapter $adapter)
+    {
+        $this->adapter = $adapter;
+    }
+
+    public function getCommands()
+    {
+
+        $adapter = $this->adapter;
+
+        return [
+            new Commands\Init($adapter),
+            new Commands\Reset($adapter),
+            new Commands\Listing($adapter),
+            new Commands\Drop($adapter),
+            new Commands\Status($adapter),
+            new Commands\Create($adapter),
+            new Commands\Up($adapter),
+            new Commands\Down($adapter),
+            new Commands\Up($adapter),
+            new Commands\About()
+        ];
+    }
+
+}

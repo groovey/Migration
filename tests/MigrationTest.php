092eb0fdfd63cd0e1d861c76dbe30ae987707066
pokoot <pokoot@gmail.com>
2016-06-26 22:50:49 +0800

    Remove unecessary test files

--- tests/MigrationTest.php
+++ tests/MigrationTest.php
@@ -1,16 +1,11 @@
 <?php
 
 use Groovey\Migration\Migration;
-use Groovey\Migration\Adapters\Mysql;
-use Groovey\Migration\Adapters\Adapter;
 
 class MigrationTest extends PHPUnit_Framework_TestCase
 {
     public function testSatus()
     {
-        $adapter     = new Adapter(new Mysql());
-        $application = new Migration($adapter);
-
         $this->assertTrue(true);
     }
 }

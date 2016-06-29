6186f59a59662e035a44989809e84e87b98aa471
pokoot <pokoot@gmail.com>
2016-06-30 05:06:33 +0800

    Added files for the sake of testing

--- tests/MigrationTest.php
+++ tests/MigrationTest.php
@@ -1,11 +1,65 @@
 <?php
 
-use Groovey\Migration\Migration;
+use Symfony\Component\Console\Application;
+use Symfony\Component\Console\Tester\CommandTester;
+use Illuminate\Database\Capsule\Manager as Capsule;
+use Groovey\Migration\Commands\About;
+use Groovey\Migration\Commands\Init;
 
 class MigrationTest extends PHPUnit_Framework_TestCase
 {
-    public function testSatus()
+    public function connect()
     {
-        $this->assertTrue(true);
+        $capsule = new Capsule();
+
+        $capsule->addConnection([
+            'driver'    => 'mysql',
+            'host'      => 'localhost',
+            'database'  => 'test',
+            'username'  => 'root',
+            'password'  => '',
+            'charset'   => 'utf8',
+            'collation' => 'utf8_general_ci',
+            'prefix'    => '',
+        ], 'default');
+
+        $capsule->bootEloquent();
+        $capsule->setAsGlobal();
+
+        return $capsule;
+    }
+
+    public function testAbout()
+    {
+        $app = new Application();
+        $app->add(new About());
+
+        $command = $app->find('migrate:about');
+
+        $tester = new CommandTester($command);
+        $tester->execute([
+                'command' => $command->getName(),
+            ]);
+
+        $this->assertRegExp('/Groovey/', $tester->getDisplay());
+    }
+
+    public function testInit()
+    {
+        $app = new Application();
+        $container['db'] = $this->connect();
+
+        $app->add(new Init($container));
+        $command = $app->find('migrate:init');
+
+        $tester = new CommandTester($command);
+        $tester->execute([
+                'command' => $command->getName(),
+            ]);
+
+        print_r( $tester->getDisplay() );
+
+
+        $this->assertRegExp('/Sucessfully/', $tester->getDisplay());
     }
 }
